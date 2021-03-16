197256bfc731d29355be663e0d6202db85875ebc
GET Academy <terje@kolderup.net>
2021-03-16 12:45:00 +0100

    x

---
+++ RectangleUltraBultra/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace RectangleUltraBultra
+{
+    class Position
+    {
+        public int X;
+        public int Y;
+
+        public Position GetLinePosition(int dy)
+        {
+            return new Position {X = X, Y = Y + dy};
+        }
+    }
+}
--- RectangleUltraBultra/Program.cs
+++ RectangleUltraBultra/Program.cs
@@ -6,8 +6,15 @@ namespace RectangleUltraBultra
     {
         static void Main(string[] args)
         {
-            var x = 5;
-            var y = 7;
+            //var position = new Position {
+            //    X = 5,
+            //    Y = 7
+            //};
+            var position = new Position();
+            position.X = 5;
+            position.Y = 7;
+            //int x = 5;
+            //int y = 7;
             var width = 10;
             var height = 4;
             var color = ConsoleColor.Green;
@@ -17,21 +24,22 @@ namespace RectangleUltraBultra
                 Console.Clear();
                 for (var dy = 0; dy < height; dy++)
                 {
-                    DrawLine(x, y + dy, color, width);
+                    var linePosition = position.GetLinePosition(dy);
+                    DrawLine(linePosition, color, width);
                 }
 
                 var key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.RightArrow) x++;
-                else if (key.Key == ConsoleKey.LeftArrow) x--;
-                else if (key.Key == ConsoleKey.UpArrow) y--;
-                else if (key.Key == ConsoleKey.DownArrow) y++;
+                if (key.Key == ConsoleKey.RightArrow) position.X++;
+                else if (key.Key == ConsoleKey.LeftArrow) position.X--;
+                else if (key.Key == ConsoleKey.UpArrow) position.Y--;
+                else if (key.Key == ConsoleKey.DownArrow) position.Y++;
             }
         }
 
-        static void DrawLine(int x, int y, ConsoleColor color, int length)
+        static void DrawLine(Position position, ConsoleColor color, int length)
         {
-            Console.CursorLeft = x;
-            Console.CursorTop = y;
+            Console.CursorLeft = position.X;
+            Console.CursorTop = position.Y;
             Console.ForegroundColor = color;
             Console.Write(string.Empty.PadLeft(length, '█'));
             Console.CursorLeft = Console.WindowWidth - 1;
