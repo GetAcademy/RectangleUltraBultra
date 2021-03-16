625e2e2f32413e511b2f9a3e07b1622e503d2c0f
GET Academy <terje@kolderup.net>
2021-03-16 13:30:20 +0100

    x

--- RectangleUltraBultra/Program.cs
+++ RectangleUltraBultra/Program.cs
@@ -14,46 +14,30 @@ namespace RectangleUltraBultra
             while (true)
             {
                 Console.Clear();
-                var height = rectangle.Position2.Y - rectangle.Position1.Y;
-                for (var dy = 0; dy < height; dy++)
-                {
-                    var linePosition = rectangle.Position1.GetLinePosition(dy);
-                    var width = rectangle.Position2.X - rectangle.Position1.X;
-                    DrawLine(linePosition, rectangle.Color, width);
-                }
+                rectangle.Draw();
 
                 var key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.RightArrow)
-                {
-                    rectangle.Position1.X++;
-                    rectangle.Position2.X++;
-                }
-                else if (key.Key == ConsoleKey.LeftArrow)
-                {
-                    rectangle.Position1.X--;
-                    rectangle.Position2.X--;
-                }
-                else if (key.Key == ConsoleKey.UpArrow)
-                {
-                    rectangle.Position1.Y--;
-                    rectangle.Position2.Y--;
-                }
-                else if (key.Key == ConsoleKey.DownArrow)
-                {
-                    rectangle.Position1.Y++;
-                    rectangle.Position2.Y++;
-                }
+                rectangle.HandleKey(key);
             }
         }
-
-        static void DrawLine(Position position, ConsoleColor color, int length)
+        
+        /* Kun én Rectangle
+        static void Main(string[] args)
         {
-            Console.CursorLeft = position.X;
-            Console.CursorTop = position.Y;
-            Console.ForegroundColor = color;
-            Console.Write(string.Empty.PadLeft(length, '█'));
-            Console.CursorLeft = Console.WindowWidth - 1;
-            Console.CursorTop = Console.WindowHeight - 1;
+            var rectangle = new Rectangle();
+            rectangle.Position1 = new Position { X = 5, Y = 7 };
+            rectangle.Position2 = new Position { X = 15, Y = 11 };
+            rectangle.Color = ConsoleColor.Green;
+
+            while (true)
+            {
+                Console.Clear();
+                rectangle.Draw();
+
+                var key = Console.ReadKey(true);
+                rectangle.HandleKey(key);
+            }
         }
+        */
     }
 }
--- RectangleUltraBultra/Rectangle.cs
+++ RectangleUltraBultra/Rectangle.cs
@@ -7,5 +7,52 @@ namespace RectangleUltraBultra
         public Position Position1;
         public Position Position2;
         public ConsoleColor Color;
+
+        public int GetWidth()
+        {
+            return Position2.X - Position1.X;
+        }
+
+        public int GetHeight()
+        {
+            return Position2.Y - Position1.Y;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            Position1.X += dx;
+            Position2.X += dx;
+            Position1.Y += dy;
+            Position2.Y += dy;
+        }
+
+        public void HandleKey(ConsoleKeyInfo key)
+        {
+            if (key.Key == ConsoleKey.RightArrow) Move(1, 0);
+            else if (key.Key == ConsoleKey.LeftArrow) Move(-1, 0);
+            else if (key.Key == ConsoleKey.UpArrow) Move(0, -1);
+            else if (key.Key == ConsoleKey.DownArrow) Move(0, 1);
+        }
+
+        public void Draw()
+        {
+            var height = GetHeight();
+            for (var dy = 0; dy < height; dy++)
+            {
+                var linePosition = Position1.GetLinePosition(dy);
+                var width = GetWidth();
+                DrawLine(linePosition, Color, width);
+            }
+        }
+
+        private static void DrawLine(Position position, ConsoleColor color, int length)
+        {
+            Console.CursorLeft = position.X;
+            Console.CursorTop = position.Y;
+            Console.ForegroundColor = color;
+            Console.Write(string.Empty.PadLeft(length, '█'));
+            Console.CursorLeft = Console.WindowWidth - 1;
+            Console.CursorTop = Console.WindowHeight - 1;
+        }
     }
 }
